ccbd819e2f7c10671e2ff503e6c0b9bd31545a83
Hieu <vuminhhieu1805@gmail.com>
2016-10-13 16:36:58 +0700

    update register
    without test :))

--- app/src/main/java/com/example/dell/myapplication/Register.java
+++ app/src/main/java/com/example/dell/myapplication/Register.java
@@ -3,10 +3,21 @@ package com.example.dell.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+import cz.msebera.android.httpclient.Header;
 
 public class Register extends AppCompatActivity {
 
@@ -20,6 +31,9 @@ public class Register extends AppCompatActivity {
     Button  btnSignUp_register;
 
     TextView txtSignUp_register;
+    TextView txtMessage_register;
+
+    LinearLayout llMessage_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,8 @@ public class Register extends AppCompatActivity {
         edtEmail        = (EditText)findViewById(R.id.edtEmail_register);
         edtPassword     = (EditText)findViewById(R.id.edtPassword_register);
         edtConfirmPassword = (EditText)findViewById(R.id.edtConfirmPassword_register);
+        txtMessage_register = (TextView)findViewById(R.id.txtMessage_register);
+        llMessage_register = (LinearLayout)findViewById(R.id.llMessage_register);
 
         btnSignUp_register = (Button)findViewById(R.id.btnSignUp_register);
         btnSignUp_register.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +67,34 @@ public class Register extends AppCompatActivity {
                 String Email        = edtEmail.getText().toString();
                 String Password     = edtPassword.getText().toString();
                 String ConfirmPassword = edtConfirmPassword.getText().toString();
+                String message = "";
+//                    llMessage_register.setVisibility(View.VISIBLE);
+//                    txtMessage_register.setText(getString(R.string.match_password));
+                RequestParams rp = new RequestParams();
+                rp.add("email", Email);
+                rp.add("firstName", FirstName);
+                rp.add("lastName", LastName);
+                rp.add("phoneNumber", PhoneNumber);
+                rp.add("organizationDestinationId", "");
+                rp.add("passwords", Password);
+                rp.add("confirmPasswords", ConfirmPassword);
+
+                HttpUtils.post("/api/v1/user/register", rp, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        Log.d("statusCode: ","" + statusCode);
+                        Log.d("headers: ", Arrays.toString(headers));
+                        Log.d("response: ",response.toString());
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        Log.d("statusCode: ","" + statusCode);
+                        Log.d("headers: ", Arrays.toString(headers));
+                        Log.d("throwable: ",throwable.toString());
+                        Log.d("errorResponse: ",errorResponse.toString());
+                    }
+                });
             }
         });
     }
